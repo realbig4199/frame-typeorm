61595b4426b56a32f96c0bf17fed4d743fa0b3c8
lilyshin <lilyshin.dev@gmail.com>
2025-04-06 12:07:07 +0900

    refactor: queryBuilder 제거, paginate 함수 사용 수정

--- src/api/board/board-custom.repository.ts
+++ src/api/board/board-custom.repository.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { Between, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BoardEntity } from '@/database/entity/board.entity';
 import { SearchCriteriaDto } from '@/common/dto/search.criteria.dto';
@@ -18,7 +18,6 @@ export class BoardCustomRepository {
 
   /**
    * 페이징을 포함한 게시글 목록을 조회합니다.
-   * @author <lilyshin.dev@gmail.com>
    * @param searchCriteria
    * @param paginationOptions
    */
@@ -29,15 +28,18 @@ export class BoardCustomRepository {
     const { startDate, endDate } = searchCriteria;
     const { page, limit } = paginationOptions;
 
-    const queryBuilder = this.boardRepository
-      .createQueryBuilder('board')
-      .leftJoinAndSelect('board.createdBy', 'user')
-      .where('board.createdAt BETWEEN :startDate AND :endDate', {
-        startDate,
-        endDate,
-      })
-      .orderBy('board.createdAt', 'DESC');
-
-    return paginate<BoardEntity>(queryBuilder, { page, limit });
+    return await paginate<BoardEntity>(
+      this.boardRepository,
+      { page, limit },
+      {
+        where: {
+          createdAt: Between(new Date(startDate), new Date(endDate)),
+        },
+        order: {
+          createdAt: 'DESC',
+        },
+        relations: ['createdBy'],
+      },
+    );
   }
 }
